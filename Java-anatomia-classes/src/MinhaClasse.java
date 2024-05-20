4cbdc2285b75519329f1eb09dcce966312ffd27a
lecoOliveiraStartDev <lecooliveirastartdev@hotmail.com>
2024-05-20 10:33:18 -0300

    Atualização

--- Java-anatomia-classes/src/MinhaClasse.java
+++ Java-anatomia-classes/src/MinhaClasse.java
@@ -4,9 +4,9 @@ Chame este arquivo de Example2.java.
 */
 class Example2 {
     public static void main(String args[]) {
-        int var1; // esta instrução declara uma variável
-        int var2; // esta instrução declara outra variável
-        var1 = 1024; // esta instrução atribui 1024 a var1
+        int var1; // esta instrução declara uma variável <- Declara variáveis
+        int var2; // esta instrução declara outra variável 
+        var1 = 1024; // esta instrução atribui 1024 a var1 <- Atribui valor a uma variável.
         System.out.println("var1 contains " + var1);
         var2 = var1 / 2;
         System.out.print("var2 contains var1 / 2: ");
