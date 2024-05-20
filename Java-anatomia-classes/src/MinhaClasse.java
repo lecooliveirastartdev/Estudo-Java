65de6ba883f524a811ded26e154f67dbcafc2098
lecoOliveiraStartDev <lecooliveirastartdev@hotmail.com>
2024-05-20 15:39:53 -0300

    20/05/2024

--- Java-anatomia-classes/src/MinhaClasse.java
+++ Java-anatomia-classes/src/MinhaClasse.java
@@ -1,18 +1,19 @@
-/*
-Este código demonstra uma variável.
-Chame este arquivo de Example2.java.
-*/
-class Example2 {
-    public static void main(String args[]) {
-        int var1; // esta instrução declara uma variável <- Declara variáveis
-        int var2; // esta instrução declara outra variável 
-        var1 = 1024; // esta instrução atribui 1024 a var1 <- Atribui valor a uma variável.
-        System.out.println("var1 contains " + var1);
-        var2 = var1 / 2;
-        System.out.print("var2 contains var1 / 2: ");
-        System.out.println(var2);
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+        String primeiroNome = "Alexandro";
+        String segundoNome = "Oliveira!";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+
+        return "Ô, filho de Deus " + primeiroNome.concat(" ").concat(segundoNome);
+
     }
 }
-// Declara variáveis.
-// Atribui um valor
-// a uma
