c4a467c8ef0168288f49a33cf03530434e64f15f
lecoOliveiraStartDev <lecooliveirastartdev@hotmail.com>
2024-05-20 15:55:43 -0300

    Indentação

---
+++ Java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+        int mediaFinal = 1;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("RECUPERAÇÃO");
+        else
+            System.out.println(("APROVADO"));
+
+    }
+}
+// Correção da INDENTAÇÃO.
