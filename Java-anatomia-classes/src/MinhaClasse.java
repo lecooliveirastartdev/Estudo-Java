734f2b1a824506b4d4b02e2ae1ecb46d66a8b0a5
lecoOliveiraStartDev <lecooliveirastartdev@hotmail.com>
2024-05-20 10:01:37 -0300

    commit inicial

---
+++ Java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,18 @@
+/*
+Este código demonstra uma variável.
+Chame este arquivo de Example2.java.
+*/
+class Example2 {
+    public static void main(String args[]) {
+        int var1; // esta instrução declara uma variável
+        int var2; // esta instrução declara outra variável
+        var1 = 1024; // esta instrução atribui 1024 a var1
+        System.out.println("var1 contains " + var1);
+        var2 = var1 / 2;
+        System.out.print("var2 contains var1 / 2: ");
+        System.out.println(var2);
+    }
+}
+// Declara variáveis.
+// Atribui um valor
+// a uma
---
+++ java-para-iniciantes/src/Example.java
@@ -0,0 +1,12 @@
+/*
+Este é um programa Java simples.
+Chame este arquivo de Example.java.
+*/
+public class Example {
+        // Um programa Java começa com uma chamada a main().
+        public static void main(String args[]) {
+
+            System.out.print("java exemplo");
+        }
+
+}
