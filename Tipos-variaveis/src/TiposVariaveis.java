1d6d13a95e873a3beff719e6c7c34849a6bd03d1
lecoOliveiraStartDev <lecooliveirastartdev@hotmail.com>
2024-05-22 17:52:31 -0300

    Update

--- Tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 =(short) numeroNormal;
+
+        /*
+		 * ao usar a palavra reservada final, você determina que jamais
+		 * esta variavel poderá obter outro valor;
+		 * logo a linha 25 vai apresentar um erro de compilação
+		 * isso é considerado uma CONSTANTE na linguagem Java
+		 */
+
+
+         final double VALOR_DE_PI = 3.14;
+
+System.out.println(VALOR_DE_PI);
+
+
+
+
     }
 }
