c26f1afed7dd63e6a9354204efd49cf8d860b753
lecoOliveiraStartDev <lecooliveirastartdev@hotmail.com>
2024-05-28 17:54:40 -0300

    commit pasta terminal

--- Terminal-E-Argumentos/src/MinhaClasse.java
+++ Terminal-E-Argumentos/src/MinhaClasse.java
@@ -1,7 +1,7 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.println("Oi,fui executado peko terminal");
+        System.out.println("Oi turma,fui executado pelo Terminal");
          
     }
     
