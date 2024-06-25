004776dbe228804a1d2c11e40f200bd52133203f
lecoOliveiraStartDev <lecooliveirastartdev@hotmail.com>
2024-06-25 08:27:48 -0300

    Novo

---
+++ Area-Teste-Java/src/CategoriaLivros.java
@@ -0,0 +1,14 @@
+import.main.List.Pesquisa]
+
+
+
+public class CategoriaLivros {
+
+    private List<Livro> livroList;
+
+    public CategoriaLivros() {
+
+        this.livroList = new ArrayList<>();
+    }
+
+}
